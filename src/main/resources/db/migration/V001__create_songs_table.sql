a9572e399f3b8557a94a34e6e32be52f033ff6fb
Tom Lambregts <tom.lambregts@kunlabora.be>
2026-06-08 14:59:45 +0200

    Revert "Add postgres and flyway"
    This reverts commit d4b8b3d8

--- src/main/resources/db/migration/V001__create_songs_table.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists songs (
-    id Uuid primary key not null,
-    title Text not null,
-    artist Text not null,
-    album Text not null,
-    duration_in_seconds Int not null
-);
